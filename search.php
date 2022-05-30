e38b5d1ace450f2fc4c71f2ed363f5f3e9046001
mymaestro <mymaestro@gmail.com>
2022-05-30 13:49:14 -0500

    updated search function

--- search.php
+++ search.php
@@ -11,34 +11,17 @@
   }
   require_once("includes/config.php");
   require_once("includes/navbar.php");
+  require_once("includes/functions.php");
 ?>
-<main role="main" class="container">
+<main role="main">
   <div class="container">
     <h1>Search the Music Library</h1>
-    <h3>Tables</h3>
-    <strong>This part is still under construction</strong>
     <p>These are the tables that are used in the music library database.
       <dl>
-        <dt>Compositions</dt>
-          <dd>Search compositions in the music library. This table holds the data about the music in the library. You find here the title, composer, arranger, publish date, etc.</dd>
-          <div id="search_form">
-              <form action="compositions.php" method="POST">
-                  <div class="row g-3 align-items-center">
-                      <div class="col-auto">
-                          <button type="submit" name="submitButton" class="btn btn-primary">Search</button>
-                      </div>
-                      <div class="col-auto">
-                          <input type="text" id="search" name="search" class="form-control" aria-describedby="searchHelp">
-                      </div>
-                      <div class="col-auto">
-                        <span id="searchHelp" class="form-text">
-                            Search the name, description, composer, arranger, and comments
-                        </span>
-                      </div>
-                  </div>
-              </form>
-          </div>
-          <dd>Show me <a href="compositions.php">everything</a> in the library</dd>
+        <dt><a href="compositions.php">Compositions</a></dt>
+          <dd>Browse the compositions in the music library. This table holds the data about the music in the library. You find here the title, composer, arranger, publish date, etc.</dd>
+        <dt><a href="parts.php">Parts</a></dt>
+          <dd>Instrument parts in the library</dd>
         <dt><a href="ensembles.php">Ensembles</a></dt>
           <dd>List ensembles. Bands and smaller ensembles</dd>
         <dt><a href="genres.php">Genres</a></dt>
@@ -55,7 +38,74 @@
           <dd>Users who can access the database, with username/password and role permissions. Roles are "administrator", "user" or both.</dd>
       </dl>
     </p>
-  </div>
+    <h2>Search compositions</h2>
+    <div id="search_form">
+      <form action="compositions.php" method="POST">
+        <div class="row g-3 align-items-center">
+          <div class="col-auto">
+            <button type="submit" name="submitButton" class="btn btn-primary">Search</button>
+          </div>
+          <div class="col-auto">
+            <input type="text" id="search" name="search" class="form-control" aria-describedby="searchHelp">
+          </div>
+          <div class="col-auto">
+            <span id="searchHelp" class="form-text">
+              Search the name, description, composer, arranger, and comments
+            </span>
+          </div>
+        </div>
+      </form>
+    </div>
+    <div class="row">
+      <div class="col-auto">
+    <?php
+    echo '<h4>Ensembles</h4>
+    <form action="compositions.php" method="POST">';
+    $f_link = f_sqlConnect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $sql = "SELECT id_ensemble, name FROM ensembles WHERE enabled = 1 ORDER BY name;";
+    $res = mysqli_query($f_link, $sql) or die('Error: ' . mysqli_error($f_link));
+    while ($rowList = mysqli_fetch_array($res)) {
+        $id_ensemble = $rowList['id_ensemble'];
+        $title = $rowList['name'];
+        echo '<div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="'.$id_ensemble.'">
+        <label class="form-check-label" for="'.$id_ensemble.'">
+          '.$title.'
+        </label>
+      </div>';
+    }
+    mysqli_close($f_link);
+    ferror_log("returned: " . $sql);
+    echo '</form>';
+?>
+    </div>
+    <div class="col-auto">
+      <?php
+    echo '<h4>Genre</h4>
+    <form action="compositions.php" method="POST">';
+    $f_link = f_sqlConnect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $sql = "SELECT id_genre, name FROM genres WHERE enabled = 1 ORDER BY name;";
+    $res = mysqli_query($f_link, $sql) or die('Error: ' . mysqli_error($f_link));
+    while ($rowList = mysqli_fetch_array($res)) {
+        $id_genre = $rowList['id_genre'];
+        $name = $rowList['name'];
+        $description = $rowList['description'];
+        echo '<div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="'.$id_genre.'">
+        <label class="form-check-label" for="'.$id_genre.'">
+          '.$name.'
+        </label>
+      </div>';
+    }
+    mysqli_close($f_link);
+    ferror_log("returned: " . $sql);
+    echo '</form>';
+?>
+    </div>
+
+    </div>
+    
+  </div><!-- CONTAINER -->
 </main>
 <?php require_once("includes/footer.php");?>
 </body>

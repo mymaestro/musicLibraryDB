aec0d5ee0ffe74ee4b38d55fa9fd8d140dc1b31d
mymaestro <mymaestro@gmail.com>
2025-06-07 11:12:51 -0500

    Create default example config file

---
+++ includes/config.example.php
@@ -0,0 +1,21 @@
+<?php
+/*
+#############################################################################
+# Licensed Materials - Property of ACWE
+# (C) Copyright Austin Civic Wind Ensemble 2020, 2022, 2025. All rights reserved.
+#############################################################################
+*/
+define('ORGNAME', '4th Wind');
+define('ORGDESC', 'Fourth Wind Wind Ensemble');
+define('ORGHOME', 'http://localhost/4thWind/musicLibraryDB/html/');
+define('ORGFILES', 'https://localhost/4thWind/musicLibraryDB/html/files/recordings/'); // with trailing slash
+define('ORGLOGO', 'images/logo_large.png');
+define('ORGMAIL', 'librarian@musicLibraryDB.com');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'musicLibraryDB');
+define('DB_USER', 'musicLibraryDB');
+define('DB_PASS', 'superS3cretPa$$wo4d');
+define('DB_CHARSET', 'utf8');
+define('REGION', 'HOME');
+define('DEBUG', 1);
+?>

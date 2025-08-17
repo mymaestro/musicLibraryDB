1bc811f687a79372fa32778e19beb544cc3e3118
mymaestro <mymaestro@gmail.com>
2025-08-17 14:21:08 -0500

    set date default in playgrams to NULL

--- setup/musicLibraryDB-core.sql
+++ setup/musicLibraryDB-core.sql
@@ -288,7 +288,7 @@ CREATE TABLE `playgrams` (
   `id_playgram` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique identifier, incremented number',
   `name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT 'Title of the playgram (concert program series); must be unique.',
   `description` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT 'Complete description of the program, with concert performance notes.',
-  `performance_date` date DEFAULT current_timestamp() COMMENT 'Date of the first planned performance of this playgram',
+  `performance_date` date DEFAULT NULL COMMENT 'Date of the first planned performance of this playgram',
   `enabled` int(11) DEFAULT NULL COMMENT 'Set to 1 if enabled, otherwise 0',
   PRIMARY KEY (`id_playgram`),
   UNIQUE KEY `title` (`name`)
